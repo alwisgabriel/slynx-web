ef13a1c287c5becf2654c2e4362c417dc0518947
Your Name <alwisgabriel@gmail.com>
2026-02-09 13:09:13 -0300

    feat: add card component

--- src/app/components/card/card.tsx
+++ src/app/components/card/card.tsx
@@ -3,20 +3,18 @@ import styles from './card.module.css';
 
 type CardProps = {
   title: string;
-  href: string;
   icon: ReactNode;
   children: ReactNode;
 };
 
-export function Card({ title, href, icon, children }: CardProps) {
+export function Card({ title, icon, children }: CardProps) {
   return (
     <div className={styles.card}>
       <div className={styles.icon}>{icon}</div>
+
       <div className={styles.baixo}>
         <h3 className={styles.title}>{title}</h3>
-        <a href={href} className={styles.link}>
-          {children}
-        </a>
+        {children}
       </div>
     </div>
   );
